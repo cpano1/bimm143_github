f38c91d7159c70f080a14ae06c6a86c59aff7141
Christeena Pano <christeenapano@Christeenas-MacBook-Air.local>
2026-05-19 12:44:16 -0700

    Added Class 4-7 files

---
+++ Lab 04/Lab04.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "Class4: Intro to R"
+#' author: "Christeena pano"
+#' date: "April 9, 2026"
+#' ---
+
+
+
+# This is just a text file
+x <- 1:50
+plot(x, sin(x), type="l", col="blue", lwd="3")
+
